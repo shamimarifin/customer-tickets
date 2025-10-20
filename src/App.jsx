304c0b4b9a212e9996745a7ab551da4bc86e8adf
shamimarifin <shamimarifin790@gmail.com>
2025-10-20 11:35:49 +0600

    Some added

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
   
   return (
     <>
+    {/* Navbar */}
       <Navbar></Navbar>
       
       <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
